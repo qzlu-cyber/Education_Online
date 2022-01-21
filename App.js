978f8fcbfca2a3823ce9e0d1ce142d2c79968d99
LiuJunQi <941648981@qq.com>
2022-01-21 18:58:42 +0800

    大体框架完成，接着做表单验证

--- App.js
+++ App.js
@@ -1,67 +1,19 @@
 /*
  * @Author: 刘俊琪
  * @Date: 2022-01-07 18:52:21
- * @LastEditTime: 2022-01-20 15:22:59
- * @Description: 描述
+ * @LastEditTime: 2022-01-21 18:57:10
+ * @Description: 程序入口
  */
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { FontAwesome5 } from "@expo/vector-icons";
 
-import CategoryScreen from "./app/screens/CategoryScreen";
-import HomeScreen from "./app/screens/HomeScreen";
-import LearningScreen from "./app/screens/LearningScreen";
-import MyScreen from "./app/screens/MyScreen";
-import OnBoardingScreen from "./app/screens/OnBoardScreen";
-import CategoryDetailScreen from "./app/screens/CategoryDetailScreen";
-import colors from "./app/config/colors";
-
-const Stack = createNativeStackNavigator();
-const Tab = createMaterialBottomTabNavigator();
-
-const TabNavigator = () => (
-  <Tab.Navigator
-    activeColor={colors.praimary}
-    inactiveColor={colors.boldText}
-    barStyle={{ backgroundColor: colors.white }}
-    screenOptions={({ route }) => ({
-      tabBarIcon: ({ focused, color, size }) => {
-        let iconName;
-        if (route.name == "首页") iconName = "home";
-        else if (route.name == "分类") iconName = "archive";
-        else if (route.name == "学习") iconName = "book-open";
-        else if (route.name == "我的") iconName = "user-alt";
-        return <FontAwesome5 name={iconName} size={size} color={color} />;
-      },
-      headerShown: false,
-    })}>
-    <Tab.Screen name='首页' component={HomeScreen} />
-    <Tab.Screen name='分类' component={CategoryScreen} />
-    <Tab.Screen name='学习' component={LearningScreen} />
-    <Tab.Screen name='我的' component={MyScreen} />
-  </Tab.Navigator>
-);
-
-const StackNavigator = () => (
-  <Stack.Navigator screenOptions={{ headerShown: false }}>
-    <Stack.Screen name='首页' component={TabNavigator} />
-    <Stack.Screen
-      name='分类详情'
-      component={CategoryDetailScreen}
-      options={({ route }) => ({
-        title: route.params.categoryName,
-        headerShown: true,
-      })}
-    />
-  </Stack.Navigator>
-);
+import AccountNavigator from "./app/navigation/AccountNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <AccountNavigator /> */}
+      <AuthNavigator />
     </NavigationContainer>
   );
 }
